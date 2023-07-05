7794b9f8d3feedf5ae6a682c3a1a8bb9e1ca02d7
MirosBL45 <jovicmiroslav88@gmail.com>
2023-07-05 19:02:52 +0200

    testing for now

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import { Button, Typography, styled } from "@mui/material";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AddIcon from "@mui/icons-material/Add";
+
 function App() {
+  const BlueButton = styled(Button)(({ theme }) => ({
+    backgroundColor: theme.palette.otherColor.main,
+    color: "#888",
+    margin: 5,
+    "&:hover": {
+      backgroundColor: "green",
+      color: "red",
+    },
+    "&:disabled": {
+      backgroundColor: "gray",
+      color: "white",
+    },
+  }));
+
   return (
     <div>
-      Starting
+      <Button variant="text">Text</Button>
+      <Button
+        startIcon={<SettingsIcon />}
+        color="secondary"
+        variant="contained"
+      >
+        Contained
+      </Button>
+      <Button startIcon={<AddIcon />} color="success" variant="contained">
+        Uspesno
+      </Button>
+      <Button variant="outlined" disabled>
+        Outlined
+      </Button>
+      <Typography variant="h1" component="p">
+        koristi h1 stil ali je p tag
+      </Typography>
+      <BlueButton>Prvo dugme</BlueButton>
+      <BlueButton>Drugo</BlueButton>
     </div>
   );
 }
