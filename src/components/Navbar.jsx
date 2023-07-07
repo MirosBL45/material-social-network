568f355a629899faa83d46eb8bee7101ee004059
MirosBL45 <jovicmiroslav88@gmail.com>
2023-07-07 18:06:13 +0200

    work navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,16 @@
-import { AppBar, Box, Toolbar, Typography, styled } from "@mui/material";
-import ImportantDevicesIcon from '@mui/icons-material/ImportantDevices';
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Toolbar,
+  Typography,
+  styled,
+} from "@mui/material";
+import ImportantDevicesIcon from "@mui/icons-material/ImportantDevices";
+import MailIcon from "@mui/icons-material/Mail";
+import { Notifications } from "@mui/icons-material";
 import React from "react";
 
 const StyledToolbar = styled(Toolbar)({
@@ -7,25 +18,37 @@ const StyledToolbar = styled(Toolbar)({
   justifyContent: "space-between",
 });
 
-const Search = styled('div')(({theme})=>({
-  backgroundColor: 'white',
-  padding: '0 10px',
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
   borderRadius: theme.shape.borderRadius,
-  width: '40%',
+  width: "40%",
 }));
 
-const Icons = styled(Box)(({theme})=>({
-  backgroundColor: 'white',
+const Icons = styled(Box)(({ theme }) => ({
+  backgroundColor: "white",
 }));
 
 function Navbar() {
   return (
     <AppBar position="sticky">
       <StyledToolbar>
-        <Typography variant="h6" sx={{display: {xs: 'none', sm: 'block'}}}>MICKEY DEV</Typography>
-        <ImportantDevicesIcon sx={{display: {xs: 'block', sm: 'none'}}} />
-        <Search>Search</Search>
-        <Icons>Icons</Icons>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          MICKEY DEV
+        </Typography>
+        <ImportantDevicesIcon sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="secondary">
+            <MailIcon />
+          </Badge>
+          <Badge badgeContent={2} color="secondary">
+            <Notifications />
+          </Badge>
+          <Avatar />
+        </Icons>
       </StyledToolbar>
     </AppBar>
   );
