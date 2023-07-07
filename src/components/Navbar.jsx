841cef24048e61c7b30b01c1235c3d12d9a15e2d
MirosBL45 <jovicmiroslav88@gmail.com>
2023-07-07 17:26:54 +0200

    Navbar in progress

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { AppBar, Box, Toolbar, Typography, styled } from "@mui/material";
+import ImportantDevicesIcon from '@mui/icons-material/ImportantDevices';
+import React from "react";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled('div')(({theme})=>({
+  backgroundColor: 'white',
+  padding: '0 10px',
+  borderRadius: theme.shape.borderRadius,
+  width: '40%',
+}));
+
+const Icons = styled(Box)(({theme})=>({
+  backgroundColor: 'white',
+}));
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{display: {xs: 'none', sm: 'block'}}}>MICKEY DEV</Typography>
+        <ImportantDevicesIcon sx={{display: {xs: 'block', sm: 'none'}}} />
+        <Search>Search</Search>
+        <Icons>Icons</Icons>
+      </StyledToolbar>
+    </AppBar>
+  );
 }
 
-export default Navbar
+export default Navbar;
