21d97f52e3d0611eb5a8acfd0d03f6711b657700
MirosBL45 <jovicmiroslav88@gmail.com>
2023-07-12 14:16:42 +0200

    posts on place

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -3,13 +3,8 @@ import React from "react";
 
 function Rightbar() {
   return (
-    <Box
-      bgcolor="lightcoral"
-      flex={2}
-      p={2}
-      sx={{ display: { xs: "none", sm: "block" } }}
-    >
-      Rightbar
+    <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <Box sx={{ position: "fixed" }}>Hello</Box>
     </Box>
   );
 }
