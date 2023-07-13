f0ec6e88f53a10a9f6122205770c9da036def87b
MirosBL45 <jovicmiroslav88@gmail.com>
2023-07-13 17:24:55 +0200

    theme starting

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,11 +1,46 @@
-import { Fab, Tooltip } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Button,
+  ButtonGroup,
+  Fab,
+  Modal,
+  Stack,
+  TextField,
+  Tooltip,
+  Typography,
+  styled,
+} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
-import React from "react";
+import React, { useState } from "react";
+import {
+  DateRange,
+  EmojiEmotions,
+  Image,
+  PersonAdd,
+  VideoCameraBack,
+} from "@mui/icons-material";
+
+const StyledModal = styled(Modal)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const UserBox = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  marginBottom: "20px",
+});
 
 function Add() {
+  const [open, setOpen] = useState(false);
+
   return (
     <div>
       <Tooltip
+        onClick={() => setOpen(true)}
         title="Add"
         sx={{
           position: "fixed",
@@ -17,6 +52,58 @@ function Add() {
           <AddIcon />
         </Fab>
       </Tooltip>
+      <StyledModal
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          width={400}
+          height={290}
+          bgcolor="whitesmoke"
+          p={3}
+          borderRadius={5}
+        >
+          <Typography variant="h6" color="gray" textAlign="center">
+            Create post
+          </Typography>
+          <UserBox>
+            <Avatar
+              sx={{ width: 30, height: 30 }}
+              alt="Remy Sharp"
+              src="/static/images/avatar/1.jpg"
+            />
+            <Typography variant="span" fontWeight={500}>
+              Mickey Dev
+            </Typography>
+          </UserBox>
+          <TextField
+            sx={{ width: "100%" }}
+            id="standard-multiline-static"
+            multiline
+            rows={1}
+            placeholder="What's on your mind"
+            variant="standard"
+          />
+          <Stack direction="row" gap={1} mt={2} mb={3}>
+            <EmojiEmotions color="primary" />
+            <Image color="secondary" />
+            <VideoCameraBack color="success" />
+            <PersonAdd color="secondary" />
+          </Stack>
+          <ButtonGroup
+            fullWidth
+            variant="contained"
+            aria-label="outlined primary button group"
+          >
+            <Button>Post</Button>
+            <Button sx={{ width: "100px" }}>
+              <DateRange />
+            </Button>
+          </ButtonGroup>
+        </Box>
+      </StyledModal>
     </div>
   );
 }
